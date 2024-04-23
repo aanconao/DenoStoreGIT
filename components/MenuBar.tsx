ee8583060c839f97a6a4cbb71ffda171b2ba7e59
aanconao <alan_ancona_ortiz@hotmail.com>
2024-04-23 10:07:56 +0200

    Cuarto Commit

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -14,7 +14,7 @@ const MenuBarComponent: FunctionComponent<BarMenuProps> = ({ BarMenu }) => {
           BarMenu.value == MenuBar.ABOUT_US;
         }}
       >
-        ABOUT_US
+        ABOUT_Us
       </div>
     </div>
   );
